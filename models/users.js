00d3e641ee8cefce1865048e80d7e1ebae32fd2d
Manish Vashisth <ebriks.manish@gmail.com>
2018-02-09 17:01:06 +0530

    changes in model but no schema changes , only style

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const userData = new Schema({
+let userData = new mongoose.Schema;
+userData = {
     username : {
         type : String
     },
@@ -10,6 +10,36 @@ const userData = new Schema({
     password : {
         type : String
     }
-});
+};
 
-const users = module.exports = mongoose.model('users', userData);
+
+
+// const Schema = mongoose.Schema;
+// userData = new mongoose.Schema({
+//     username : {
+//         type : String
+//     },
+//     email : {
+//         type : String
+//     },
+//     password : {
+//         type : String
+//     }
+// });
+
+// const Schema = mongoose.Schema;
+// const userData = new Schema({
+//     username : {
+//         type : String
+//     },
+//     email : {
+//         type : String
+//     },
+//     password : {
+//         type : String
+//     }
+// });
+
+module.exports = mongoose.model('users', userData);
+
+// const users = module.exports = mongoose.model('users', userData);
